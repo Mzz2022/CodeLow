b043455ba825cc44f4a92617a26ebd8e460788a0
Mzz2022 <1556435206@qq.com>
2024-12-23 17:58:29 +0800

    feat:zustand init

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,3 +1,37 @@
+import { useEffect } from "react";
+import { useComponetsStore } from "../stores/components";
 export function EditArea() {
-  return <div>EditArea</div>;
+  const { components, addComponent, deleteComponent, updateComponentProps } =
+    useComponetsStore();
+  useEffect(() => {
+    addComponent(
+      {
+        id: 222,
+        name: "Container",
+        props: {},
+        children: [],
+      },
+      1
+    );
+    addComponent(
+      {
+        id: 333,
+        name: "Video",
+        props: {},
+        children: [],
+      },
+      222
+    );
+    updateComponentProps(222, {
+      title: "6666",
+    });
+    // setTimeout(() => {
+    //     deleteComponent(333);
+    // }, 3000);
+  }, []);
+  return (
+    <div>
+      <pre>{JSON.stringify(components, null, 2)}</pre>
+    </div>
+  );
 }
