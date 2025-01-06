a9aca24a66d3fb2d1651df21c7d4c3037d9ef226
songhua.mo <songhua.mo@mintegral.com>
2025-01-06 19:33:08 +0800

    【新增功能】hover 高亮框和组件名

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+```shell
+npm run dev
+```
+
 allotment 实现可拖动改变大小的 pane
--- src/editor/components/EditArea/index.tsx
+++ src/editor/components/EditArea/index.tsx
@@ -1,37 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // @ts-ignore
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useComponentConfigStore } from "../../stores/component-config";
 import { Component, useComponetsStore } from "../../stores/components";
+import HoverMask from "../HoverMask";
 
 export function EditArea() {
   // @ts-ignore
   const { components, addComponent } = useComponetsStore();
   const { componentConfig } = useComponentConfigStore();
-
-  // useEffect(() => {
-  //   addComponent(
-  //     {
-  //       id: 222,
-  //       name: "Container",
-  //       props: {},
-  //       children: [],
-  //     },
-  //     1
-  //   );
-
-  //   addComponent(
-  //     {
-  //       id: 333,
-  //       name: "Button",
-  //       props: {
-  //         text: "无敌",
-  //       },
-  //       children: [],
-  //     },
-  //     222
-  //   );
-  // }, []);
 
   function renderComponents(components: Component[]): React.ReactNode {
     return components.map((component: Component) => {
@@ -54,11 +31,39 @@ export function EditArea() {
       );
     });
   }
+  const [hoverComponentId, setHoverComponentId] = useState<number>();
 
+  const handleMouseOver: MouseEventHandler = (e) => {
+    const path = e.nativeEvent.composedPath();
+
+    for (let i = 0; i < path.length; i += 1) {
+      const ele = path[i] as HTMLElement;
+
+      const componentId = ele.dataset?.componentId;
+      if (componentId) {
+        setHoverComponentId(+componentId);
+        return;
+      }
+    }
+  };
   return (
-    <div className="h-[100%]">
-      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+    <div
+      className="h-[100%] edit-area"
+      onMouseOver={handleMouseOver}
+      onMouseLeave={() => {
+        setHoverComponentId(undefined);
+      }}
+      // onClick={handleClick}
+    >
       {renderComponents(components)}
+      {hoverComponentId && (
+        <HoverMask
+          portalWrapperClassName="portal-wrapper"
+          containerClassName="edit-area"
+          componentId={hoverComponentId}
+        />
+      )}
+      <div className="portal-wrapper"></div>
     </div>
   );
 }
